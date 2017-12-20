40e3dc186a1439470b5873ad6e6b6201b4e1f5c3
flygyyy <792211491@qq.com>
2017-12-20 17:03:26 +0800

    First commit

---
+++ torch/csrc/Storage.cpp
@@ -0,0 +1,18 @@
+#define __STDC_FORMAT_MACROS
+
+#include <Python.h>
+#include <structmember.h>
+
+#define THP_HOST_HALF
+
+#include <stdbool.h>
+#include <TH/TH.h>
+#include <libshm.h>
+#include "THP.h"
+#include "copy_utils.h"
+
+#include "generic/Storage.cpp"
+#include <TH/THGenerateAllTypes.h>
+
+#include "generic/Storage.cpp"
+#include <TH/THGenerateHalfType.h>
